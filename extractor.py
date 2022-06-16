b8ed5fd29ff23c53d2917bde009dd8efcc44460c
PatrickHoeveler <patrick.hoeveler@gmx.de>
2022-06-16 10:46:54 +0200

    added fft

--- extractor.py
+++ extractor.py
@@ -3,9 +3,10 @@ import wave
 import numpy as np
 import matplotlib.pyplot as plt
 
-filename = 'song-lowpass-100.wav'
 
+filename = 'song.wav'
 
+# filename = 'song-lowpass-100.wav'
 # filename='song-midpass-100-5000.wav'
 # filename='song-highpass-5000.wav'
 # filename='kick.wav'
@@ -18,8 +19,13 @@ print(signal_wave.getparams())
 
 duration = n_frames/frame_rate
 # print(duration)
+window = frame_rate
+# window = 10000
+print(window)
 
-signal = np.frombuffer(signal_wave.readframes(n_frames), dtype=np.int16)
+
+# get 1 second of signal
+signal = np.frombuffer(signal_wave.readframes(window), dtype=np.int16)
 
 # sig = sig[0:1000]
 # print(signal)
@@ -29,32 +35,51 @@ left, right = signal[0::2], signal[1::2]
 # print(right)
 # fft_spectrum = np.fft.rfft(signal)
 
-# plt.figure(figsize=(10,6))
+# move from time to frequency domain
+fft = np.fft.fft(left)
+
+# phase and magnitude (complex number)
+print(fft[0])
+magnitude_spectrum = np.abs(fft)
+print(magnitude_spectrum[0])
 
 
-max_left = max(left)
+def plot_magnitue_spectrum(signal, sr, f_ratio=1):
+    ft = np.fft.fft(signal)
+    magnitude_spectrum = np.abs(ft)
+    frequency = np.linspace(0, sr, len(magnitude_spectrum))
+    num_frequency_bins = int(len(frequency) * f_ratio)
+
+    plt.figure(figsize=(10,6))
+    plt.plot(frequency[:num_frequency_bins], magnitude_spectrum[:num_frequency_bins])
+    plt.xlabel('Frequency [Hz]')
+    plt.ylabel('Magnitude')
+    plt.show()
+
+
+plot_magnitue_spectrum(left, frame_rate, 0.05)
+
+# max_left = max(left)
 # left = left[1900000:2200000]
 
 # left = [0 if x < 0 else x for x in left]
-print(len(left))
-threshold = max_left -max_left*0.25
-print(max_left)
-print(threshold)
+# print(len(left))
+# threshold = max_left -max_left*0.25
+# print(max_left)
+# print(threshold)
 
 
-bass_hits = [int(max_left) if x > threshold else 0 for x in left]
+# bass_hits = [int(max_left) if x > threshold else 0 for x in left]
 
-print(len([x for x in bass_hits if x > 0]))
+# print(len([x for x in bass_hits if x > 0]))
 
-with open('bass_hits.json', 'w') as file:
-    json.dump(bass_hits, file)
+# with open('bass_hits.json', 'w') as file:
+    # json.dump(bass_hits, file)
 
 # plot_a = plt.subplot(211)
-# # # plot_a.plot(fft_spectrum)
 # plot_a.plot(left)
 # plot_a.plot(bass_hits)
 # # plot_a.plot(right)
 
 # plot_a.set_xlabel('frames')
 # plot_a.set_ylabel('amplitude')
-# plt.show()
